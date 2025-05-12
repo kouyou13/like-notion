6e49f5bd440b42e12984daac1b043c016fae9258
kouyou13 <legacy1113v@gmail.com>
2025-05-12 21:35:08 +0900

    時間の表記を修正

--- src/features/TopBarMutate/utils/calcMinutesSinceEdited.ts
+++ src/features/TopBarMutate/utils/calcMinutesSinceEdited.ts
@@ -6,19 +6,19 @@ const calcMinutesSinceEdited = (page: Page | undefined) => {
   if (page?.updatedAt) {
     const now = dayjs()
     const targetPageUpdatedAt = page.updatedAt
-    const diff = targetPageUpdatedAt.diff(now) / 1000 //秒単位
+    const diff = now.diff(targetPageUpdatedAt) / 1000 //秒単位
     if (diff < 60) {
       // 1分以内
       return 'たった今 編集'
     } else if (diff < 3600) {
       // 1時間以内
-      return `${String(diff / 60)}分前 編集`
+      return `${String(Math.trunc(diff / 60))}分前 編集`
     } else if (diff < 86400) {
       // 1日以内
-      return `${String(diff / 3600)}時間前 編集`
+      return `${String(Math.trunc(diff / 3600))}時間前 編集`
     } else {
       // 1日以上前
-      return `${String(diff / 86400)}日前 編集`
+      return `${String(Math.trunc(diff / 86400))}日前 編集`
     }
   }
   return ''
